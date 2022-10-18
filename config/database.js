ea8be2f3ac0d239a5d2692c9b4ad0eb5767cff2d
Natan Cardoso <natansc73@gmail.com>
2022-10-18 20:12:15 -0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,28 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'postgres'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', '@@123abc'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: false,
+    },
+    debug: false,
+  },
+});
+
+
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
